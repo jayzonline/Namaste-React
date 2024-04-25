e9cf79eb39eb8931f687327124ec553e199ac0ab
jayagovind <jayagovind.e@p2dl.com>
2024-04-25 13:37:32 +0530

    Episode 6.2 using corsProxy for api call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,4 @@
 import ResturentCard from "./ResturentCard";
-//import ResObj , {RealObj} from '../utils/mockdata'
 import { useState, useEffect } from "react";
 import Shimmer from "./shimmer";
 import Footer from "./Footer";
@@ -32,6 +31,7 @@ export const Body = () => {
     }, [])
 
     const fetchData = async () => {
+        //https://corsproxy.io/?
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=D');
         const json = await data.json();
         const openResturentdata = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
